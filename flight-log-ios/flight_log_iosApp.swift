ccab87bf072108feb728cad63869b464757bf7cb
Johannes Scribante <johannes.scribs@gmail.com>
2022-12-24 16:21:59 +0200

    initial commit

---
+++ flight-log-ios/flight_log_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_log_iosApp.swift
+//  flight-log-ios
+//
+//  Created by Johannes Scribante on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct flight_log_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
